ea53b656974b691b193ce1f4bf3acafb8103abbf
rbtroselli <rbt.roselli@gmail.com>
2024-03-30 03:12:54 +0100

    reorg

--- src/_config.py
+++ src/_config.py
@@ -1,15 +1,17 @@
 from pathlib import Path
+
+# other files
 from _keys import MAPQUEST_API_KEY
 from _geocoder_exceptions import GEOCODER_EXCEPTION_DICT
 
-current_dir = Path(__file__).resolve().parent
-
 # paths
-DATABASE_FOLDER_PATH = current_dir.parent / 'database' 
+current_dir = Path(__file__).resolve().parent
+DB_FOLDER_PATH = current_dir.parent / 'database' 
 DDL_FOLDER_PATH = current_dir.parent / 'database' / 'ddl'
 LOG_FOLDER_PATH = current_dir.parent / 'logs'
 API_KEYS_FILE_PATH = current_dir / 'keys.py'
 
+# flags
 DEBUG_MODE = False
 
 # print(DATABASE_PATH / 'test.db')
