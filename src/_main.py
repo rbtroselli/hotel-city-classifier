310f7735aeb019ee120456479c0b619658efdcdd
rbtroselli <rbt.roselli@gmail.com>
2024-04-01 22:39:05 +0200

    switch to subclass for hotel iterator

--- src/_main.py
+++ src/_main.py
@@ -1,8 +1,9 @@
 from base_iterator import BaseIterator
 from result_iterator import ResultIterator
+from hotel_iterator_2 import HotelIterator
 from review_iterator import ReviewIterator
 
 
 if __name__ == '__main__':
-    bi = ReviewIterator(test=False, log_file_name='review_iterator.log', db_name='hotel.db', db_connection=True, browser_driver=True)
+    bi = HotelIterator(test=True, log_file_name='hotel_iterator.log', db_name='test.db', db_connection=True, browser_driver=True)
     bi.run()
